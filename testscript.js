611bf1f32413cdaf4fe5ff51fc32720785c43458
colourlessblueideas <djoverton@gmail.com>
2013-04-24 00:04:01 -0300

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,7 +1,5 @@
-$(document).ready(
-  function(){
-  $("div").click(
-    function(){
-      $("div").fadeOut("slow",0.2);
+$(document).ready(function() {
+    $('div').click(function() {
+        $(this).fadeOut('slow');
     });
 });
